83d1b2c0d715bdd2b619b62236a12f3567555157
Gitgoogoo-source <googoohztd2022@gmail.com>
2026-01-14 21:02:50 +0800

    44

--- public/index.299b7.js
+++ public/index.299b7.js
@@ -1,4 +1,4 @@
-System.register(["./application.717bb.js"], function (_export, _context) {
+System.register(["./application.b0873.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
