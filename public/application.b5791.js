56040e34c9f70f95687d4b18bc252a14304bda1b
Gitgoogoo-source <googoohztd2022@gmail.com>
2026-01-15 16:47:56 +0800

    777

--- public/application.b5791.js
+++ public/application.b5791.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.9c4cf.json';
+          this.settingsPath = 'src/settings.f3e8a.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- public/index.426b5.js
+++ public/index.426b5.js
@@ -1,4 +1,4 @@
-System.register(["./application.98d4f.js"], function (_export, _context) {
+System.register(["./application.b5791.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
